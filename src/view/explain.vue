08c58bf3b38ea9ef8995c47a724f26a858e1f5d6
huangjia <5263954@qq.com>
2018-09-22 11:55:32 +0800

    update version

--- src/view/explain.vue
+++ src/view/explain.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="explain">
-<div class="top">
+    <div class="top">
       <h2>增加中国农历、24节气和节假日的功能</h2>
       <h2>npm install vue-lunar-full-calendar</h2>
       <div>A vue component for lunar fullcalendar. Uses Moment.js for date operations.（Increase the functions of Chinese lunar calendar, 24 solar terms and holidays）</div>
       <div class="red">window.lunar(date) 使用此依赖包可以使用这个功能获取农历的信息date是时间日期</div>
       <div>其它功能可以参照<a href="https://fullcalendar.io/docs" target="_blank">https://fullcalendar.io/docs</a> 这个链接为官方的API文档去熟悉</div>
-     </div>
-     <img class="lunarImg" src="@/assets/img/lunar.png" alt="">
-     
-     <div>
-         <span class="back" @click="back">back</span>
-     </div>
-</div>
+    </div>
+    <img class="lunarImg" src="@/assets/img/lunar.png" alt="">
+    <div>
+        <span class="back" @click="back">back</span>
+    </div>
+  </div>
 </template>
 <script>
 /* eslint-disable */
